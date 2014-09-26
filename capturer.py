f43b14e223bc49acdc51f0ff3447f36d29dd8f47
Roberto Salas <wala.rsalas@gmail.com>
2014-09-26 17:14:58 -0300

    fix in the lastest mitmproxy git

--- capturer.py
+++ capturer.py
@@ -1,6 +1,6 @@
 import os,re
 
 def request(context, flow):
-  if(re.search(".*--.*\\.googlevideo\\.com", flow.request.get_host())):
-        url="%s%s" % (flow.request.get_host(), flow.request.path)
+  if(re.search(".*--.*\\.googlevideo\\.com", flow.request.host)):
+        url="%s%s" % (flow.request.host, flow.request.path)
         os.system("echo \"https://%s\"|pbcopy" % url)
